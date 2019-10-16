888b1073f78023b22fb1883a67bcaa634aa85ef0
Eric Ferguson <ferguson.eric.r@gmail.com>
2019-10-16 14:37:16 -0700

    ADDED -- xp button

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ function App() {
   const [homeScore, setHomeScore] = useState(0);
   const [awayScore, setAwayScore] = useState(0);
 
+  //home and away teams made dynamic
+  const homeTeam = 'Salt'; 
+  const awayTeam = 'Slugs';
+
   return (
     <div className="container">
       <section className="scoreboard">
         <div className="topRow">
           <div className="home">
-            <h2 className="home__name">Lions</h2>
+            <h2 className="home__name">{homeTeam}</h2>
 
             {/* TODO STEP 3 - We need to change the hardcoded values in these divs to accept dynamic values from our state. */}
 
@@ -22,7 +26,7 @@ function App() {
           </div>
           <div className="timer">00:03</div>
           <div className="away">
-            <h2 className="away__name">Tigers</h2>
+            <h2 className="away__name">{awayTeam}</h2>
             <div className="away__score">{awayScore}</div>
           </div>
         </div>
@@ -30,12 +34,13 @@ function App() {
       </section>
       <section className="buttons">
         <div className="homeButtons">
-          {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
           <button className="homeButtons__touchdown" onClick={()=>setHomeScore(homeScore+6)}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={()=>setHomeScore(homeScore+1)}>Home Extra Point</button>
           <button className="homeButtons__fieldGoal" onClick={()=> setHomeScore(homeScore+3)}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
           <button className="awayButtons__touchdown" onClick={()=> setAwayScore(awayScore+6)}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={()=> setAwayScore(awayScore+1)}>Away Extra Point</button>
           <button className="awayButtons__fieldGoal" onClick={()=>setAwayScore(awayScore+3)}>Away Field Goal</button>
         </div>
       </section>
